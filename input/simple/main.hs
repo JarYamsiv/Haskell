c921563d15fe72d035930a7194b125520810b459
JarYamsiv <vismayraj1@gmail.com>
2019-03-21 21:28:23 +0530

    added few more tips

--- input/simple/main.hs
+++ input/simple/main.hs
@@ -3,13 +3,12 @@ main =  do
   input1 <- getLine
   putStrLn "enter value for y: " 
   input2 <- getLine 
-  input3 <- getLine
 
   let x = (read input1 :: Int)
   let y = (read input2 :: Int)
 
   
-
+  --cannot end a do block with let it will generate some error
   return ()
 
 
