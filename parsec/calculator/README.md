2d1721d8d523d88877e5b2d71bb41f96a45c0083
JarYamsiv <vismayraj1@gmail.com>
2019-04-21 01:55:27 +0530

    added the calculator project

---
+++ parsec/calculator/README.md
@@ -0,0 +1,8 @@
+# Basic Calculator
+
+## Follows the youtube tutorial:
+https://www.youtube.com/watch?v=9AllRc64pVE
+
+## Interesting things that i found
+cabal install parsec
+to install parsec library
---
+++ parsec/calculator/calculator.hs
@@ -0,0 +1,25 @@
+import Text.Parsec
+import Text.Parsec.String
+
+parseNumber :: Parser Int
+parseNumber = do
+  -- the <|> means that try what is there on my left if that fails then try my right
+  	--char and all are parsec provided functions
+  neg <- (char '-' >> return "-") <|> (return "")
+  n' <- many1 $ oneOf "0123456789"
+  return (read  (neg ++ n') )
+
+calculation :: Parser Int
+calculation = do
+  parseNumber
+
+calculate :: String -> String
+calculate s = 
+  case ret of
+    Left e -> "error: " ++ (show e)
+    Right n -> "answer: " ++ (show n)
+  where
+    ret = parse calculation "" s
+
+main :: IO ()
+main = interact (unlines . (map calculate) . lines)
