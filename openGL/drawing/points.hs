ae65766c809a89ce89903a6dc06414b08a111703
vismay <JarYamsiv>
2020-04-28 15:13:18 +0530

    added user input to opengl code

--- openGL/drawing/points.hs
+++ openGL/drawing/points.hs
@@ -6,14 +6,17 @@ myPoints x = [ (sin (2*pi*k/x), cos (2*pi*k/x), 0) | k <- [1..x] ]
 
 main :: IO ()
 main = do
+  putStrLn "enter the number of points"
+  input1 <- getLine
+  let x = (read input1 :: Float)
   (_progName, _args) <- getArgsAndInitialize
   _window <- createWindow "Hello World"
-  displayCallback $= display
+  displayCallback $= display x
   mainLoop
 
-display :: DisplayCallback
-display = do 
+display ::Float -> DisplayCallback
+display x = do 
   clear [ColorBuffer]
   renderPrimitive Polygon $ mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) $ pts
   flush
-  where pts = myPoints 100
+  where pts = myPoints x
